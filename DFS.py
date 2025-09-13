6a12353a918b21cbe2d8fd2aba382c47c3a1106a
lakshyachaudhry <lakshchaudhry@gmail.com>
2025-09-13 18:41:06 -0400

    Add depth-first search implementations: inorder, preorder, and postorder traversals

---
+++ DFS.py
@@ -0,0 +1,24 @@
+#this file is for my DFS implementations
+#DFS can be implemented 3 ways, in-order, pre-order and post-order
+
+def inorder_traversal(root):
+    if root == None:
+        return None
+    inorder_traversal(root.left)
+    print(root.val)
+    inorder_traversal(root.right)
+
+def preorder_traversal(root):
+    if root == None:
+        return None
+    print(root.val)
+    preorder_traversal(root.left)
+    preorder_traversal(root.right)
+
+
+def postorder_traversal(root):
+    if root == None:
+        return None
+    postorder_traversal(root.left)
+    postorder_traversal(root.right)
+    print(root.val)
