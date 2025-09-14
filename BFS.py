facde343518fa6963529eb623b9571d956cd8518
lakshyachaudhry <lakshchaudhry@gmail.com>
2025-09-13 20:03:08 -0400

    Add breadth-first search traversal implementation

---
+++ BFS.py
@@ -0,0 +1,19 @@
+#this is for bfs traversal
+
+from collections import deque
+
+def bfs_traversal(root):
+    if root == None:
+        return None
+    
+    queue = deque()
+    if root:
+        queue.append(root)
+    
+    while len(queue) > 0:
+        current = queue.popleft()
+        print(current.val)
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
