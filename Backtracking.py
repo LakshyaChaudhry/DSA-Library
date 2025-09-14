eb7188c58fda94200f6339275da77de508d4a896
lakshyachaudhry <lakshchaudhry@gmail.com>
2025-09-14 17:39:27 -0400

    backtracking implementation with path sum logic

--- Backtracking.py
+++ Backtracking.py
@@ -1,2 +1,13 @@
-# This file is for a backtracking implementation
+# This file is for a backtracking implementation of a path sum problem
+
+def hasPathSum(root, targetSum):
+    if root == None:
+        return False
+        
+    if root.left == None and root.right == None:
+        return targetSum == root.val
+        
+    left = hasPathSum(root.left, targetSum - root.val)
+    right = hasPathSum(root.right, targetSum - root.val)
+    return left or right
 
