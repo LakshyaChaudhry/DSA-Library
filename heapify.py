0ed1fac5b2f90636b40bc6aeb79851438dc2275b
lakshyachaudhry <lakshchaudhry@gmail.com>
2025-10-02 15:02:36 -0400

    Fix heapify loop to include the root node and add newline at end of file

--- heapify.py
+++ heapify.py
@@ -3,8 +3,32 @@
 def heapify(A):
     n = len(A)
 
-    for i in range((n // 2 - 1), 0, -1):
+    for i in range((n // 2 - 1), -1, -1):
         sift_down(A, n, i)
 
 def sift_down(A, n, i):
+    m = minChildIndex(A, n, i)
+    while m < n and A[m] < A[i]:
+        swap(A, i, m)
+        i = m
+        m = minChildIndex(A, n, i)
+
+def minChildIndex(A, n, i):
+    left = 2*i + 1
+    right = 2*i + 2
+
+    if left >= n:
+        return n
     
+    if right >= n:
+        return left
+    else:
+        if A[left] < A[right]:
+            return left
+        else:
+            return right
+        
+def swap(A, i, j):
+    A[i], A[j] = A[j], A[i]
+        
+
