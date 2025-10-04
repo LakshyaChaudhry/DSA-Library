90da75c0b38486152e8be8cbfb027ee21c6b1db5
lakshyachaudhry <lakshchaudhry@gmail.com>
2025-10-04 13:08:10 -0400

    Add implementation of climbStairs function for dynamic programming solution

---
+++ 1-dDynamicProgramming.py
@@ -0,0 +1,12 @@
+#this file is the code implementation of the neetcode stairs dynamic programming problem
+
+def climbStairs(n):
+    if n <= 1:
+        return 1
+    dp = [0] * (n+1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
